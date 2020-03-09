4a8b7bc746f658cfbae824e05f10c34b012fc2b8
xiaoxiao <1028514892@qq.com>
2020-03-09 10:03:24 +0800

    index and bootstrap

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,12 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.annotation.Resources;
 
 /**
  * @author xiaoxin
@@ -15,10 +10,8 @@ import javax.annotation.Resources;
 @Controller
 public class UserController {
     @GetMapping("/hello")
-    public String aa(@RequestParam("name") String name, Model model){
-        model.addAttribute("name",name);
-        model.addAttribute("key","key");
+    public String aa(){
 
-        return "hello";
+        return "index";
     }
 }
